e3d0ff0d64a1f82d177132df7f9f9c567613c828
Alex Jung <alexander-jung@uiowa.edu>
2021-11-28 16:33:58 -0600

    Firebase integration started
    Co-authored-by: rdfarwell <rdeanfarwell3@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,21 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import { getUsers } from "../utils/users"
 
-const IndexPage = () => (
-  <Layout>
-    <Link to="/TestFormik/">Test Form Page</Link> <br/>
-    <Link to="/createPoll/">Create Poll</Link> <br/>
-    <Link to="/TestCalendar/">Test Calendar</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  React.useEffect(() => {
+    getUsers()
+  }, [])
+  
+
+  return (
+    <Layout>
+      <Link to="/TestFormik/">Test Form Page</Link> <br/>
+      <Link to="/createPoll/">Create Poll</Link> <br/>
+      <Link to="/TestCalendar/">Test Calendar</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
