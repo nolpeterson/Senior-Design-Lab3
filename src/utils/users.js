fda671aced6395c9defddaee2ea960b6cfe0d295
Dean Farwell <rdeanfarwell3@gmail.com>
2021-11-29 17:33:42 -0600

    Added getUserID to users util
    Gets the id of the document of the passed username, needed for retrieving polls
    
    Co-Authored-By: Alex Jung <73133056+awjung@users.noreply.github.com>

--- src/utils/users.js
+++ src/utils/users.js
@@ -2,10 +2,13 @@ import { firestore } from "./firebase"
 
 const getUsers = async () => {
     const snapshot = await firestore.collection("users").get()
-    snapshot.docs.forEach(doc => console.log(doc.data()))
+    let users = []
+    snapshot.docs.forEach(doc => users.push(doc.data()))
+    users.forEach(user => console.log(user))
+    return users
 }
 
-const setUsers = async (mail, first, last, pass, user) => {
+const setUser = async (mail, first, last, pass, user) => {
     const userDb = firestore.collection('users').doc()
     await userDb.set({
         email: mail,
@@ -16,4 +19,10 @@ const setUsers = async (mail, first, last, pass, user) => {
     });
 }
 
-export {getUsers, setUsers}
+const getUserID = async (username) => {
+    const snapshot = await firestore.collection('users').where('username', '==', username).get()
+    snapshot.docs.forEach(doc => console.log(doc.id))
+    snapshot.docs.forEach(doc => {return doc.id})
+}
+
+export {getUsers, setUser, getUserID}
