f1c284ed4ec11d4de61413b96da2d9187e3bcc28
Alex Jung <alexander-jung@uiowa.edu>
2021-12-01 01:18:35 -0600

    Added back button to testCalender

--- src/pages/TestCalendar.js
+++ src/pages/TestCalendar.js
@@ -4,6 +4,7 @@ import moment from 'moment'
 import Layout from "../components/layout"
 import events from '../events'
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Link } from "@reach/router"
 
 const localizer = momentLocalizer(moment) // or globalizeLocalizer
 
@@ -23,6 +24,10 @@ const MyCalendar = () => {
                 startAccessor="start"
                 endAccessor="end"
                 />
+            </div>
+
+            <div>
+                <Link to="/dashboard">Back to dashboard</Link>
             </div>
         </Layout>
     )
