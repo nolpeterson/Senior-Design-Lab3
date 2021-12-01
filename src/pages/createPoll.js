91c3bcfdb32867e4d71556ab761d4758461daef9
nolpeterson <nolpeterson@uiowa.edu>
2021-12-01 14:58:25 -0600

    Integrated calendar into create poll page

--- src/pages/createPoll.js
+++ src/pages/createPoll.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage  } from 'formik';
 import { Link } from "gatsby"
 import Layout from '../components/layout';
@@ -7,6 +7,11 @@ import { getUser } from '../services/auth';
 import { getPollID, setPoll } from '../utils/polls';
 import { getUserID } from '../utils/users';
 import { navigate } from '@reach/router';
+import { Calendar , momentLocalizer} from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const PollSchema = Yup.object().shape({
   title: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
@@ -21,6 +26,32 @@ const PollSchema = Yup.object().shape({
   startingDate: Yup.string().required('Required'),
   endingDate: Yup.string().required('Required'),
 });
+
+const events = [{}]
+const localizer = momentLocalizer(moment)
+
+function App() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+      setAllEvents([...allEvents, newEvent]);
+  }
+
+  return (
+    <div className="calendar">
+        <div>
+            <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+            <DatePicker showTimeSelect dateFormat="MM/dd/yyyy h:mm aa" timeIntervals={5} placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+            <DatePicker showTimeSelect dateFormat="MM/dd/yyyy h:mm aa" timeIntervals={5} placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+            <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                Add Event
+            </button>
+        </div>
+        <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+    </div>
+  );
+}
 
 const Basic = () => (
   <Layout>
@@ -95,7 +126,9 @@ const Basic = () => (
             </ErrorMessage>
           </div>
 
-          <div name = "selectionDiv">
+          <App/>
+
+          {/* <div name = "selectionDiv">
             <br/>
             <label htmlFor="numDays">Number of days for poll</label>
             <Field type="number" id="numDays" name="numDays"/>
@@ -130,7 +163,7 @@ const Basic = () => (
             <ErrorMessage name="endingDate">
               { msg => <div style={{ color: 'red' }}>{msg}</div> }
             </ErrorMessage>
-          </div>
+          </div> */}
           
           <div name = "votesTimeDiv">
             <br/>
