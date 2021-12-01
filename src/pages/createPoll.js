5d1d948d1c5ed96fa74ad6deed3cb02a849bed1d
nolpeterson <nolpeterson@uiowa.edu>
2021-11-30 19:32:44 -0600

    Added more features to poll creation

--- src/pages/createPoll.js
+++ src/pages/createPoll.js
@@ -6,14 +6,18 @@ import * as Yup from 'yup';
 import { getUser } from '../services/auth';
 import { setPoll } from '../utils/polls';
 
-const timezones = ["Eastern", "Central", "Mountain", "Pacific"];
-
 const PollSchema = Yup.object().shape({
-  title: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required(),
+  title: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
   location: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!'),
-  // timezone: Yup.string().required().oneOf(timezones),
-  votesPerTimeslot: Yup.number().min(1, 'Minimum votes are 1'),
-  votesPerUser: Yup.number().min(1, 'Minimum votes are 1')
+  timezone: Yup.string().required('Required'),
+  deadline: Yup.string().required('Required'),
+  votesPerTimeslot: Yup.number().required('Required').min(1, 'Minimum votes are 1'),
+  votesPerUser: Yup.number().required('Required').min(1, 'Minimum votes are 1'),
+  numDays: Yup.number().required('Required').min(1, 'Minimum days is 1'),
+  numEvents: Yup.number().required('Required').min(1, 'Minimum events is 1'),
+  lengthEvents: Yup.number().required('Required').min(5, 'Minimum length is 5 minutes'),
+  startingDate: Yup.string().required('Required'),
+  endingDate: Yup.string().required('Required'),
 });
 
 const Basic = () => (
@@ -25,90 +29,137 @@ const Basic = () => (
       <Formik
         initialValues={{
           username: getUser().username,
+          title: '',
+          location: '',
+          notes: '',
+          timezone: '',
+          deadline: '',
+          votesPerTimeslot: '',
+          votesPerUser: '',
+          numDays: '1',
+          numEvents: '1',
+          lengthEvents: '5',
+          startingDate: '',
+          endingDate: '',
         }}
         validationSchema={PollSchema}
         onSubmit={async (values) => {
           console.log(values); // Log the poll schema to console
-          if (Object.keys(values).length != 8) {
-            alert(JSON.stringify("please fill out all form sections", null, 2));
-          } else {
-            var date = new Date(values.deadline)
-            setPoll(date, values.location, values.notes, values.username, values.timezone, values.title, values.votesPerTimeslot, values.votesPerUser)
-            alert(JSON.stringify(values, null, 2));
-          }
+          var date = new Date(values.deadline)
+          setPoll(date, values.location, values.notes, values.username, values.timezone, values.title, values.votesPerTimeslot, values.votesPerUser, values.numDays, values.numEvents, values.lengthEvents, values.startingDate, values.endingDate)
+          alert(JSON.stringify(values, null, 2));
         }}
       > 
       {({ errors, touched }) => (
         <Form>
           <div name = "titleDiv"> 
-          <label htmlFor="title">Poll Title</label>
-          <Field id="title" name="title"/>
-          <ErrorMessage name="title">
-          { msg => <div style={{ color: 'red' }}>{msg}</div> }
-          </ErrorMessage>
+            <label htmlFor="title">Poll Title</label>
+            <Field id="title" name="title"/>
+            <ErrorMessage name="title">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
           </div> 
 
           <div name = "locationDiv">
-          <br/> 
-          <label htmlFor="location">Location</label>
-          <Field id="location" name="location"/>
+            <br/> 
+            <label htmlFor="location">Location</label>
+            <Field id="location" name="location" placeholder="optional"/>
           </div> 
 
           <div name = "notesDiv"> 
-          <br/>
-          <label htmlFor="notes">Notes</label>
-          <Field id="notes" name="notes"/>
+            <br/>
+            <label htmlFor="notes">Notes</label>
+            <Field id="notes" name="notes" placeholder="optional"/>
           </div>
 
           <div name = "timezone div">
-          <br/>
-          <label htmlFor="timezone">Timezone</label>
-          <Field as="select" name="timezone" id="timezone">
-            <option value=""></option>
-            <option value="None">None</option>
-            <option value="Eastern">Eastern</option>
-            <option value="Central">Central</option>
-            <option value="Mountain">Mountain</option>
-            <option value="Pacific">Pacific</option>
-          </Field>
-          <ErrorMessage name="timezone"/>
+            <br/>
+            <label htmlFor="timezone">Timezone</label>
+            <Field as="select" name="timezone" id="timezone">
+              <option value=""></option>
+              <option value="Eastern">Eastern</option>
+              <option value="Central">Central</option>
+              <option value="Mountain">Mountain</option>
+              <option value="Pacific">Pacific</option>
+            </Field>
+            <ErrorMessage name="timezone">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+          </div>
+
+          <div name = "selectionDiv">
+            <br/>
+            <label htmlFor="numDays">Number of days for poll</label>
+            <Field type="number" id="numDays" name="numDays"/>
+            <ErrorMessage name="numDays">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+
+            <br/>
+            <label htmlFor="numEvents">Number of events</label>
+            <Field type="number" id="numEvents" name="numEvents"/>
+            <ErrorMessage name="numEvents">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+
+            <br/>
+            <label htmlFor="lengthEvents">Length of each event</label>
+            <Field type="number" id="lengthEvents" name="lengthEvents"/>
+            <ErrorMessage name="lengthEvents">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+
+            <br/>
+            <label htmlFor="startingDate">Poll starting date</label>
+            <Field type="date" id="startingDate" name="startingDate"/>
+            <ErrorMessage name="startingDate">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+
+            <br/>
+            <label htmlFor="endingDate">Poll ending date</label>
+            <Field type="date" min = "startingDate" id="endingDate" name="endingDate"/>
+            <ErrorMessage name="endingDate">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+          </div>
+          
+          <div name = "votesTimeDiv">
+            <br/>
+            <label htmlFor="votesPerTimeslot">Number of votes Per Timeslot</label>
+            <Field type="number" id="votesPerTimeslot" name="votesPerTimeslot"/>
+            <ErrorMessage name="votesPerTimeslot">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
+          </div>
+
+          <div name = "votesUserDiv">
+            <br/>
+            <label htmlFor="votesPerUser">Number of votes Per User</label>
+            <Field type="number" id="votesPerUser" name="votesPerUser"/>
+            <ErrorMessage name="votesPerUser">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
           </div>
 
           <div name = "deadline div">
             <br/>
-            <label htmlFor="deadline">Deadline</label>
+            <label htmlFor="deadline">Poll voting deadline</label>
             <Field type="datetime-local" id="deadline" name="deadline"/>
-            <br/>
-          </div>
-
-          <div name = "selectionDiv" style={{color: "red"}}>
-          <br/>
-          "The poll should allow for selection of many dates and times"
-          </div>
-          
-          <div name = "votesTimeDiv">
-          <br/>
-          <label htmlFor="votesPerTimeslot">Votes Per Timeslot</label>
-          <Field type="number" id="votesPerTimeslot" name="votesPerTimeslot"/>
-          <ErrorMessage name="votesPerTimeslot"/>
-          </div>
-
-          <div name = "votesUserDiv">
-          <br/>
-          <label htmlFor="votesPerUser">Votes Per User</label>
-          <Field type="number" id="votesPerUser" name="votesPerUser"/>
-          <ErrorMessage name="votesPerUser"/>
+            <ErrorMessage name="deadline">
+              { msg => <div style={{ color: 'red' }}>{msg}</div> }
+            </ErrorMessage>
           </div>
 
           <div name = "inviteDiv">
-          <br/>
-          <button>Invite Users</button>
+            <br/>
+            <button>Invite Users</button>
           </div>
 
           <div name = "pubAndCancelDiv">
-          <br/>
-          <button type="submit">Publish Poll</button>
-          <Link to="/dashboard">Cancel</Link>
+            <br/>
+            <button type="submit">Publish Poll</button>
+            <Link to="/dashboard">Cancel</Link>
           </div>
         </Form>
         )}
