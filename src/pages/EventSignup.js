f5481703d93db1791b434c93eb4f1c070dbb7f49
Dean Farwell <rdeanfarwell3@gmail.com>
2021-12-03 01:36:16 -0600

    fix bug?

--- src/pages/EventSignup.js
+++ src/pages/EventSignup.js
@@ -5,43 +5,28 @@ import Table from "../components/eventTable";
 import { getParameterByName } from "../utils/url";
 import { getPoll } from "../utils/polls";
 
-const Dashboard = () => {
-  var title = getParameterByName('title')
-  var owner = getParameterByName('owner_id')
+const Dashboard = () => (
+  <Layout>
+      <div style={{margin: `0 auto`,maxWidth: 960}}>
 
-  var pollData 
-
-  React.useEffect(async () => {
-    pollData = await getPoll(owner, title)
-    console.log(pollData.notes)
-    pollData = pollData.notes
-  })
-
-  return (
-    <Layout>
-        <div style={{margin: `0 auto`,maxWidth: 960}}>
-
-        {/* "Signup text" */}
-        <div style={{ float: "left" }}>
-          <h1>Signup</h1>
-          <div dangerouslySetInnerHTML={{ __html: `Title: ${title}` }} />
-          <div dangerouslySetInnerHTML={{ __html: `Owner: ${owner}` }} />
-        </div>
-
+      {/* "Signup text" */}
+      <div style={{ float: "left" }}>
+        <h1>Signup</h1>
       </div>
 
-      <br/>
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-        <Link style= {{marginTop: "20px"}} to="/dashboard">Go back to dashboard</Link>
-      </div>
+    </div>
 
-      {/* List of events on a poll */}
-      <div>
-        <Table></Table>
-      </div>
-      
-    </Layout>
-  )
-}
+    <br/>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+      <Link style= {{marginTop: "20px"}} to="/dashboard">Go back to dashboard</Link>
+    </div>
+
+    {/* List of events on a poll */}
+    <div>
+      <Table></Table>
+    </div>
+    
+  </Layout>
+)
 
 export default Dashboard;
